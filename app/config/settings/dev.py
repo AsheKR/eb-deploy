c737f02f82710f17d04d53de651356954b208a26
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-05 23:36:13 -0800

    Logging 구현

--- app/config/settings/dev.py
+++ app/config/settings/dev.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = True
+DEBUG = False
 
 
 WSGI_APPLICATION = 'config.wsgi.dev.application'
@@ -11,3 +11,40 @@ DATABASES = DEV_JSON['DATABASES']
 
 # Amazon S3
 AWS_STORAGE_BUCKET_NAME = 'm41d-dev'
+
+# Logging
+LOG_DIR = os.path.join(ROOT_DIR, '.log')
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR, exist_ok=True)
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(levelname)s] %(name)s (%(asctime)s)\n\t%(message)s',
+        }
+    },
+    'handlers': {
+        'file_error': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'ERROR',
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'formatter': 'default',
+            # 최대 1MB를 넘게되면 새 파일을 만들어 저장
+            'maxBytes': 1048576,
+            # 최대 파일은 10개
+            'backupCount': 10,
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'INFO',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file_error', 'console'],
+            'level': 'INFO',
+            'propagate': True,
+        }
+    }
+}
